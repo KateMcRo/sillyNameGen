fc38a57dbcce46af17185de97aa061b9150fce1c
KateMcRo <katemcr0@gmail.com>
2023-02-21 16:49:42 -0800

    created and logged randomize functions.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,38 @@
-// Event listener
+var randomizeBtn = document.querySelector("#randomize");
+
 
 // Variables (first, last, sillyName)
+var firstName = ["Basketball ", "Boulevard ", "Bandana ", "Biblical ", "Bahamas ", "Bronchitis ", "Bottomless ", "Badminton ", "Barnacle ", "Bystander "]
+var lastName = ["Chipotle", "Cabala", "Cathedral", "Chancellor", "Companion", "Commonwealth", "Customer", "Counterpart", "Corridor", "Celibate"]
+
+var sillyName = ""
 
 // Functions (randomize selections, add together, return/print)
+function writeName () {
+    randomFirst ()
+    randomLast ()
+      
+}
+
+function firstPick () {
+
+
+}
+
+// Functions to randomize selects
+function randomFirst () {
+    var randomizeFirst = Math.floor(Math.random() * firstName.length)
+    var index = randomizeFirst
+    var chosenFirst = firstName[index] 
+    console.log(chosenFirst)   
+}
+
+function randomLast () {
+    var randomizeLast = Math.floor(Math.random() * lastName.length)
+    var index = randomizeLast
+    var chosenLast = lastName[index] 
+    console.log(chosenLast)    
+}
+
+// Event listener
+randomizeBtn.addEventListener("click", writeName)
